2cd92fb0b7ee127f824cc6b8011ff7d24efb6778
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ ✯ <ngtienhungg.work@gmail.com>
2024-10-27 01:29:02 +0700

    Auto destroy object

--- Assets/_GameDemo/Scripts/Fruit.cs
+++ Assets/_GameDemo/Scripts/Fruit.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    public bool autoDestroy;
     public float lifeTime = 5f;
     public float maxTorque = 10f;
 
@@ -14,7 +15,10 @@ public class Fruit : MonoBehaviour
 
     private void Start()
     {
-        Destroy(gameObject, lifeTime);
+        if (autoDestroy)
+        {
+            Destroy(gameObject, lifeTime);
+        }
     }
 
     public void AddForce(Vector2 force, ForceMode forceMode = ForceMode.Impulse)
@@ -22,11 +26,11 @@ public class Fruit : MonoBehaviour
         rb.AddForce(force, forceMode);
 
         // Thêm lực xoay ngẫu nhiên để fruit tự xoay
-        Vector3 randomTorque = new Vector3(
-            Random.Range(-maxTorque, maxTorque), // Xoay ngẫu nhiên quanh trục X
-            Random.Range(-maxTorque, maxTorque), // Xoay ngẫu nhiên quanh trục Y
-            Random.Range(-maxTorque, maxTorque) // Xoay ngẫu nhiên quanh trục Z
-        );
-        rb.AddTorque(randomTorque, forceMode);
+        // Vector3 randomTorque = new Vector3(
+        //     Random.Range(-maxTorque, maxTorque), // Xoay ngẫu nhiên quanh trục X
+        //     Random.Range(-maxTorque, maxTorque), // Xoay ngẫu nhiên quanh trục Y
+        //     Random.Range(-maxTorque, maxTorque) // Xoay ngẫu nhiên quanh trục Z
+        // );
+        // rb.AddTorque(randomTorque, forceMode);
     }
 }
