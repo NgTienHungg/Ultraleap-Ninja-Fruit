4457d8c8cf57af62c16c3fea84770c419a419b20
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ <ngtienhungg.work@gmail.com>
2024-11-01 18:57:10 +0700

    Game Config

---
+++ Assets/_GameDemo/Scripts/Common/GameConfig.Address.cs
@@ -0,0 +1,10 @@
+namespace Game.NinjaFruit
+{
+    public static class GameConfig
+    {
+        public static class Address
+        {
+            public const string FruitPrefab = "Fruits/Fruit_{0}";
+        }
+    }
+}
