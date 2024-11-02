c5d60e082419dcabe7e44c0e8e1433215369e62d
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ ✯ <ngtienhungg.work@gmail.com>
2024-11-02 10:48:11 +0700

    Fx spawner

---
+++ Assets/_GameDemo/Scripts/Ingame/FxSpawner.cs
@@ -0,0 +1,18 @@
+using BaseSource;
+using UnityEngine;
+
+namespace NinjaFruit
+{
+    public class FxSpawner : MonoSingleton<FxSpawner>
+    {
+        public ParticleSystem fxFruitSlicedPrefab;
+
+        protected override void OnAwake() { }
+
+        public void SpawnFx(Vector3 position, float lifeTime = 5f)
+        {
+            var fx = Instantiate(fxFruitSlicedPrefab, position, Quaternion.identity);
+            Destroy(fx.gameObject, lifeTime);
+        }
+    }
+}
--- Assets/_GameDemo/Scripts/Ingame/HandTouchFruit.cs
+++ Assets/_GameDemo/Scripts/Ingame/HandTouchFruit.cs
@@ -4,20 +4,65 @@ namespace NinjaFruit
 {
     public class HandTouchFruit : MonoBehaviour
     {
+        private Vector3 startPoint;
+        private Vector3 endPoint; 
+        private bool isDragging;
+
         private void Update()
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    startPoint = hit.point;
+                }
+            }
+
+            if (Input.GetMouseButton(0) && isDragging)
             {
                 var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
 
-                if (Physics.Raycast(ray, out hit))
+                if (Physics.Raycast(ray, out RaycastHit hit))
                 {
-                    if (hit.collider.CompareTag(GameConfig.Tag.FRUIT))
-                    {
-                        Destroy(hit.collider.gameObject);
-                    }
+                    endPoint = hit.point;
+                    DrawAndCheckCollision();
                 }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+            }
+        }
+
+        private void DrawAndCheckCollision()
+        {
+            // Vẽ đường từ startPoint đến endPoint
+            Debug.DrawLine(startPoint, endPoint, Color.red);
+
+            // Kiểm tra va chạm với fruit dọc theo đường kẻ
+            var direction = endPoint - startPoint;
+            var distance = direction.magnitude;
+
+            if (Physics.Raycast(startPoint, direction.normalized, out RaycastHit hit, distance))
+            {
+                if (hit.collider.CompareTag(GameConfig.Tag.FRUIT))
+                {
+                    Debug.Log("Hit fruit: " + hit.collider.name);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (isDragging)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(startPoint, endPoint);
             }
         }
     }
