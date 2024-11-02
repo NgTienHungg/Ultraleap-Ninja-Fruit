6dd2aa5b2f7fb30d25969e44bef28264a5397d8d
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ ✯ <ngtienhungg.work@gmail.com>
2024-11-02 11:29:48 +0700

    Add fx explosion in bomb

--- Assets/_NịnaFruit/Scripts/Bomb.cs
+++ Assets/_NịnaFruit/Scripts/Bomb.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    public ParticleSystem fxExplosion;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             GetComponent<Collider>().enabled = false;
+            fxExplosion.transform.SetParent(null);
+            fxExplosion.Play();
+            gameObject.SetActive(false);
+            
             GameManager.Instance.Explode();
         }
     }
-
 }
