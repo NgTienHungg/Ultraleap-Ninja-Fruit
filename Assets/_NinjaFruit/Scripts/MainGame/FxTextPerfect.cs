e761f17dc6a93a126776707f44df1c6b6466cf48
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ ✯ <ngtienhungg.work@gmail.com>
2024-12-05 22:32:27 +0700

    Sửa vị trí + góc xoay text perfect

--- Assets/_NinjaFruit/Scripts/MainGame/FxTextPerfect.cs
+++ Assets/_NinjaFruit/Scripts/MainGame/FxTextPerfect.cs
@@ -4,12 +4,18 @@ namespace NinjaFruit
 {
     public class FxTextPerfect : MonoBehaviour
     {
-        // [SerializeField] private float angle = -15f;
-        // [SerializeField] private float localZ = -5f;
+        public float offsetZ = 1f;
+        public Vector2 angleRange = new Vector2(-60f, 60f); // goc hop le
+        public Vector2 clampAngleRange = new Vector2(-30f, 30f); // goc se clamp ve
 
         public void Setup(Vector3 position, float angle)
         {
-            transform.position = position;
+            transform.position = position + new Vector3(0, 0, offsetZ);
+
+            angle = angle.InRange(angleRange.x, angleRange.y)
+                ? Mathf.Clamp(angle, clampAngleRange.x, clampAngleRange.y)
+                : 0f; // khi chem doc qua thi coi nhu goc ngang
+
             transform.localEulerAngles = new Vector3(0, 0, angle);
         }
 
