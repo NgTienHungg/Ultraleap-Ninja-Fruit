4d759508da2eb2ca8f7d486477c13fb2cbe53b96
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ ✯ <ngtienhungg.work@gmail.com>
2024-12-20 00:36:21 +0700

    Fix UI Manager

--- Assets/_NinjaFruit/Scripts/GameController.cs
+++ Assets/_NinjaFruit/Scripts/GameController.cs
@@ -2,7 +2,6 @@ using BaseSource;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace NinjaFruit
 {
@@ -10,37 +9,24 @@ namespace NinjaFruit
     public class GameController : MonoSingleton<GameController>
     {
         public CameraCtrl cameraCtrl;
-        [SerializeField] private Blade blade;
-        [SerializeField] private FruitSpawner fruitSpawner;
-        [SerializeField] private Image fadeImage;
-
-        [Space]
-        public GameObject uiHome;
-        public GameObject uiMainGame;
+        public Blade blade;
+        public FruitSpawner fruitSpawner;
 
         protected override void OnAwake() { }
 
         private void Start()
         {
-            OpenHome();
-        }
-
-        private void OpenHome()
-        {
-            uiHome.SetActive(true);
-            uiMainGame.SetActive(false);
+            UIManager.Instance.OpenHome();
         }
 
         public void NewGame()
         {
-            uiHome.SetActive(false);
-            uiMainGame.SetActive(true);
-
             Time.timeScale = 1f;
+            UIManager.Instance.ShowUIMainGame();
             LifeManager.Instance.ResetLife();
             ScoreManager.Instance.ResetScore();
-            ClearScene();
 
+            ClearScene();
             blade.enabled = true;
             fruitSpawner.enabled = true;
             StartCoroutine(fruitSpawner.Spawn());
@@ -72,16 +58,11 @@ namespace NinjaFruit
 
         private async void ExplodeSequence()
         {
-            await DOVirtual.Float(1f, 0.2f, 1f, (t) => Time.timeScale = t)
-                .SetEase(Ease.OutCubic).SetUpdate(true).ToUniTask();
-            await fadeImage.DOColor(Color.white, 0.5f)
-                .SetUpdate(true).ToUniTask();
+            await DOVirtual.Float(1f, 0.2f, 1f, (t) => Time.timeScale = t).SetEase(Ease.OutCubic).SetUpdate(true).ToUniTask();
+            await UIManager.Instance.FadeIn();
             await UniTask.Delay(200, ignoreTimeScale: true);
-
-            OpenHome();
-
-            await fadeImage.DOColor(Color.clear, 0.5f)
-                .SetUpdate(true).ToUniTask();
+            await UIManager.Instance.FadeOut();
+            UIManager.Instance.OpenHome();
         }
     }
 }
---
+++ Assets/_NinjaFruit/Scripts/UIManager.cs
@@ -0,0 +1,49 @@
+using BaseSource;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NinjaFruit
+{
+    public class UIManager : MonoSingleton<UIManager>
+    {
+        public GameObject uiHome;
+        public GameObject uiMainGame;
+        public Image fadeImage;
+        public TextMeshProUGUI txtBestScore;
+
+        protected override void OnAwake() { }
+
+        public void OpenHome()
+        {
+            uiHome.SetActive(true);
+            uiMainGame.SetActive(false);
+            txtBestScore.text = ScoreManager.BestScore.ToString();
+        }
+
+        public void ShowUIMainGame()
+        {
+            uiHome.SetActive(false);
+            uiMainGame.SetActive(true);
+        }
+
+        public void OnButtonPlay()
+        {
+            GameController.Instance.NewGame();
+        }
+
+        public UniTask FadeIn()
+        {
+            return fadeImage.DOColor(Color.white, 0.5f)
+                .SetUpdate(true).ToUniTask();
+        }
+
+        public UniTask FadeOut()
+        {
+            return fadeImage.DOColor(Color.clear, 0.5f)
+                .SetUpdate(true).ToUniTask();
+        }
+    }
+}
